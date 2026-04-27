0f202b2c67d2808a6e9133ed9e3fc7975b8f1c89
Samir Esager <sam.esa.mtf@gmail.com>
2026-04-27 08:46:26 +0200

    Add Konto class for bank account management

---
+++ Kongo.java
@@ -0,0 +1,29 @@
+public class Konto {
+    private String inhaber;
+    private double kontostand;
+
+    public Konto(String inhaber, double startbetrag) {
+        this.inhaber = inhaber;
+        this.kontostand = startbetrag;
+    }
+
+    public void einzahlen(double betrag) {
+        kontostand += betrag;
+    }
+
+    public void abheben(double betrag) {
+        if (betrag <= kontostand) {
+            kontostand -= betrag;
+        } else {
+            System.out.println("Nicht genug Geld!");
+        }
+    }
+
+    public double getKontostand() {
+        return kontostand;
+    }
+
+    public String getInhaber() {
+        return inhaber;
+    }
+}
